3dd5d68ec9588f132b1d55ffb4e7965b7372be09
loway <loway@MacBook-Pro.local>
2021-01-21 13:55:32 +0800

    [bugfix]fix list button highlight in toolbar when selecting multi listitems.

--- modules/toolbar.js
+++ modules/toolbar.js
@@ -140,9 +140,8 @@ class Toolbar extends Module {
           (formats[format] != null &&
             formats[format].toString() === input.getAttribute('value')) ||
           (formats[format] == null && !input.getAttribute('value')) ||
-          !!(format === 'list' && formats['list'] && formats['list']['list'] === input.getAttribute('value'));
-        console.log(`${format} is active: ${isActive}`);
-        console.log(formats);
+          !!(format === 'list' && formats['list'] && formats['list']['list'] === input.getAttribute('value')) ||
+          !!(format === 'list' && formats['list'] && formats['list'].length && formats['list'].every(item => item.list === input.getAttribute('value')));
         input.classList.toggle('ql-active', isActive);
       } else {
         input.classList.toggle('ql-active', formats[format] != null);
